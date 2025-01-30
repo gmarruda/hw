12d64bb168f70fbe3c894933455576abe94f10fa
Gustavo Migliorini Arruda <gmarruda@gmail.com>
2025-01-30 00:11:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def home():
-    return 'Hello, Azure!'
+def serve_home():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
